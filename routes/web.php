dc67b35541e4151a1d51c05ef8a3fe54d1f15fdd
paviweediyabandaraweerakoon <maduweediyabandaraweerakoon@gmail.com>
2026-03-03 11:38:40 +0530

    update routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
+// routes/web.php path eke thiyena file eka
 
-use App\Core\Router;
+use App\Controllers\ProductController;
+use App\Controllers\DashboardController;
+use App\Controllers\AuthController;
 
-$router = new Router();
+// Home/Dashboard route
+$router->get('/dashboard', [DashboardController::class, 'index']);
 
-// define routes here
+// Product Management Routes
+$router->get('/products', [ProductController::class, 'index']);          // Show All Products
+$router->get('/products/create', [ProductController::class, 'create']);  // See Form 
+$router->post('/products/store', [ProductController::class, 'store']);   // Database save 
+$router->get('/products/edit/{id}', [ProductController::class, 'edit']); // Edit form 
+$router->post('/products/update/{id}', [ProductController::class, 'update']); // Database update
+$router->get('/products/delete/{id}', [ProductController::class, 'delete']); // Delete 
